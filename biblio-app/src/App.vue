8ad6944875386593106aaa86b2d052b91ecf6d28
Mateus Ferreira <mreisf.contato@gmail.com>
2020-05-30 02:59:05 -0300

    Add suavidade na navegação

--- biblio-app/src/App.vue
+++ biblio-app/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition
+      name="fade"
+      mode="out-in"
+      @beforeLeave="beforeLeave"
+      @enter="enter"
+      @afterEnter="afterEnter"
+    >
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -12,5 +20,10 @@ export default {
 </script>
 
 <style>
-
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .3s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0.6;
+  }
 </style>
